958637ff0935a43642210760f3d6c156ab9cebdf
LoveShader <zy1483480718@gmail.com>
2026-05-14 23:22:45 +0800

    Is the Item Stackable

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -19,6 +19,10 @@ struct INVENTORY_API FInv_ItemManifest
 	template<typename T>
 	requires(std::derived_from<T, FInv_ItemFragment>)
 	const T* GetFragmentOfTypeWithTag(const FGameplayTag& Tag) const;
+
+	template<typename T>
+	requires(std::derived_from<T, FInv_ItemFragment>)
+	const T* GetFragmentOfType() const;
 private:
 	//Declare the item's type
 	UPROPERTY(EditAnywhere, Category = "Inventory")
@@ -41,6 +45,19 @@ template<typename T>
 		{
 			if (!FragmentPtr->GetFragmentTag().MatchesTagExact(Tag))
 				continue;
+			return FragmentPtr;
+		}
+	}
+	return nullptr;
+}
+
+template <typename T> requires (std::derived_from<T, FInv_ItemFragment>)
+const T* FInv_ItemManifest::GetFragmentOfType() const
+{
+	for (const TInstancedStruct<FInv_ItemFragment>& Fragment : Fragments)
+	{
+		if (const T* FragmentPtr = Fragment.GetPtr<T>())
+		{
 			return FragmentPtr;
 		}
 	}
